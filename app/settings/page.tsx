b224e5175f78356bc69e32d60fbc9f681333f0ab
John David L. Perez <tribrix23@gmail.com>
2026-04-15 13:42:01 +0800

    update

---
+++ app/settings/page.tsx
@@ -0,0 +1,12 @@
+import MobileSettings from '@/components/screen/mobile/settingsComponents/MobileSettings'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="w-full h-full md:hidden">
+        <MobileSettings/>
+    </div>
+  )
+}
+
+export default page
---
+++ components/screen/mobile/settingsComponents/MobileSettings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SellerAplly from './components/SellerAplly'
+
+const MobileSettings = () => {
+  return (
+    <div className='w-full h-full flex flex-col relative overflow-hidden'>
+        <SellerAplly/>
+    </div>
+  )
+}
+
+export default MobileSettings
